5bc4bd59abd4d4395780d9b8bc80b31521f22154
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-05-11 01:18:28 +0600

    read the data  from mongodb and some styles in the client site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useLoaderData } from 'react-router-dom'
 import './App.css'
+import CoffeeCart from './components/CoffeeCart';
 
 function App() {
-
+  // load data form the server
+  const coffees = useLoaderData();
   return (
-    <>
-      <h1 className='text-6xl text-purple-600'>Coffee Store</h1>
-    </>
+    <div className='m-20'>
+      <h1 className='text-6xl my-20 text-center text-purple-600'>
+        Hot Hot Cold Coffee: {coffees.length}
+      </h1>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          coffees.map(coffee => <CoffeeCart
+            key={coffee._id}
+            coffee={coffee}
+          ></CoffeeCart>)
+        }
+      </div>
+    </div>
   )
 }
 
