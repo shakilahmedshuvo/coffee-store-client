e90acc60e29876fd5813cc42c600b6fc39117f9b
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-05-11 17:48:58 +0600

    add dynamic route for update coffee and delete from mongodb

--- src/components/CoffeeCart.jsx
+++ src/components/CoffeeCart.jsx
@@ -1,7 +1,49 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CoffeeCart = ({ coffee }) => {
 
-    const { name, quantity, supplier, taste, category, details, photo } = coffee;
+    const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
+
+    // handle the delete operation start
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        // sweet alert start
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                // delete main 
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your Coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+        // sweet alert end
+    }
+    // handle the delete operation end
+
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure>
@@ -17,8 +59,14 @@ const CoffeeCart = ({ coffee }) => {
                 <div className="card-actions justify-end">
                     <div className="btn-group btn-group-vertical space-y-4">
                         <button className="btn">View</button>
-                        <button className="btn">Edit</button>
-                        <button className="btn">Delete</button>
+                        <Link to={`updateCoffee/${_id}`}>
+                            <button className="btn w-full">Edit</button>
+                        </Link>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn bg-orange-500">
+                            Delete
+                        </button>
                     </div>
                 </div>
             </div>
